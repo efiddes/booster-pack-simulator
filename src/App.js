44da5838194365bf4a975beb6df3ed802c485b56
Ethan Fiddes <efiddes@hotmail.co.uk>
2019-12-21 16:16:01 +0000

    Building out a method for building my own booster packs as the api method is lacking

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 
+import './App.css';
 import Card from './components/Card';
 
 function App() {
@@ -13,16 +13,39 @@ function App() {
   }, []);
 
   const getBooster = async () => {
-    const response = await fetch('https://api.magicthegathering.io/v1/sets/war/booster');
-    const data = await response.json();
-    console.log(data.cards);
-    setBooster(data.cards);
+    //booster api call is inadequate 
+    //const response = await fetch('https://api.magicthegathering.io/v1/sets/war/booster');
+
+    let set = 'war';
+    let rarity = Math.random() > 0.125 ? 'rare' : 'mythic';
+
+
+    const rare = await fetch(`https://api.magicthegathering.io/v1/cards?set=${set}&rarity=${rarity}&pageSize=1&random=true&contains=imageUrl`);
+    console.log(`https://api.magicthegathering.io/v1/cards?set=${set}&rarity=${rarity}&pageSize=1&random=true&contains=imageUrl`);
+    const uncommon = await fetch(`https://api.magicthegathering.io/v1/cards?set=${set}&rarity=uncommon&pageSize=3&random=true&contains=imageUrl`);
+    const common = await fetch(`https://api.magicthegathering.io/v1/cards?set=${set}&rarity=common&pageSize=10&random=true&contains=imageUrl`);
+    const basic = await fetch(`https://api.magicthegathering.io/v1/cards?set=${set}&type=basic&pageSize=1&random=true&contains=imageUrl`);
+    
+    const rares = await rare.json();
+    const uncommons = await uncommon.json();
+    const commons = await common.json();
+    const basics = await basic.json();
+
+    console.log(rares.cards);
+    console.log(uncommons.cards);
+    console.log(commons.cards);
+    console.log(basics.cards);
+
+    //setBooster(rares.cards);
+    setBooster([...rares.cards, ...uncommons.cards, ...commons.cards, ...basics.cards]);
+    //setBooster(commons.cards);
+    //setBooster(basics.cards);
   }
 
   return (
     <div className="App">
       <header>
-        <h2>Booster Pack</h2>
+        <h2>Booster Pack Simulator</h2>
       </header>
       <div className="filters">
         Filter Options:
@@ -32,6 +55,7 @@ function App() {
           key={card.number} 
           name={card.name} 
           image={card.imageUrl}
+          number={card.number}
         />
       ))}
       <footer>By Ethan Fiddes</footer>
