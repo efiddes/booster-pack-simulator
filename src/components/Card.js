175d59d74adcc1a7f2bbe4ecf526c7e235eb2831
Ethan Fiddes <efiddes@hotmail.co.uk>
2019-12-21 16:15:06 +0000

    added card back fail safe image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import style from './card.module.css';
 
-const Card = ({name, image}) => {
+import style from './card.module.css';
+import back from '../images/mtg-back.jpg';
+
+const Card = ({name, image, number}) => {
     return (
         <div className={style.card}>
-            <img className={style.image} src={image} alt={name}/>
+            <img className={style.image} src={image ? image : back} alt={name} number={number}/>
         </div>
     );
 }
