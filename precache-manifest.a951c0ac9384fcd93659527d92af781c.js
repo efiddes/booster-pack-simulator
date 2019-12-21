13e0e4648468a06888770a02b0161b66a9f80a67
Ethan Fiddes <efiddes@hotmail.co.uk>
2019-12-21 16:32:23 +0000

    Updates

---
+++ precache-manifest.a951c0ac9384fcd93659527d92af781c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e7d0cb735180360280abfac6931d60",
+    "url": "/booster-pack-simulator/index.html"
+  },
+  {
+    "revision": "61e8fdbe85f42e1daea1",
+    "url": "/booster-pack-simulator/static/css/main.4c7e05f7.chunk.css"
+  },
+  {
+    "revision": "50b205d4f090d242a3c8",
+    "url": "/booster-pack-simulator/static/js/2.33a6fa9c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/booster-pack-simulator/static/js/2.33a6fa9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61e8fdbe85f42e1daea1",
+    "url": "/booster-pack-simulator/static/js/main.3ad5c854.chunk.js"
+  },
+  {
+    "revision": "0ebfd88b780d1f68b3ec",
+    "url": "/booster-pack-simulator/static/js/runtime-main.e6bd7cd4.js"
+  },
+  {
+    "revision": "2ca7bd5a63de6dfdf12d10e4c17d6262",
+    "url": "/booster-pack-simulator/static/media/mtg-back.2ca7bd5a.jpg"
+  }
+]);
