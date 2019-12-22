f2a338b3c9f42f1a552f24c8133ddf256d5563a3
Ethan Fiddes <efiddes@hotmail.co.uk>
2019-12-22 04:01:28 +0000

    added elements for card back and front and onclick flip functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import style from './card.module.css';
+import './card.css';
 import back from '../images/mtg-back.jpg';
 
 const Card = ({name, image, number}) => {
+
+    const [flipped, setFlipped] = useState(false);
+
+    const flipOver = () => setFlipped(true);
+
     return (
-        <div className={style.card}>
-            <img className={style.image} src={image ? image : back} alt={name} number={number}/>
+        <div className={flipped ? "card flipped" : "card"} onClick={flipOver}>
+            <div className="cardBack">
+                <img className={flipped ? "image" : "image hover"} src={back} alt={name} number={number} />
+            </div>
+            <div className="cardFront">
+                <img className="image" src={image ? image : back} alt={name} number={number} />
+            </div>
         </div>
     );
 }
