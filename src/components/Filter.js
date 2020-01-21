f6d0c1bacd3a3941101d305c37b534aa1207ba8a
Ethan Fiddes <efiddes@hotmail.co.uk>
2020-01-21 02:04:41 +0000

    added some structure to the filters

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
 
+import './filter.css';
+
 
 const Filter = () => {
 
     return (
         <div className="filters">
-            Filter Options:
+            <h3>Filter Options:</h3>
+            <ul className="filter-list">
+                <li>
+                    <input type="checkbox" name="rares" defaultChecked/>
+                    <label for="rares">Mythics &amp; Rares</label>
+                    <input type="text" name="rare-quant" value="1" disabled/>
+                </li>
+                <li>
+                    <input type="checkbox" name="rares" defaultChecked/>
+                    <label for="rares">Uncommons</label>
+                    <input type="text" name="uncommon-quant" value="3" disabled/>
+                </li>
+                <li>
+                    <input type="checkbox" name="rares" defaultChecked/>
+                    <label for="rares">Commons</label>
+                    <input type="text" name="common-quant" value="10" disabled/>
+                </li>
+                <li>
+                    <input type="checkbox" name="rares" defaultChecked/>
+                    <label for="rares">Basic Lands</label>
+                    <input type="text" name="basic-quant" value="1" disabled/>
+                </li>
+            </ul> 
         </div>
     )
 }
