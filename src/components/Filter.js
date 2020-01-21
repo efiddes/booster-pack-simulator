6add1d4b227262dbafb0a88c23296a400bf5ce2f
Ethan Fiddes <efiddes@hotmail.co.uk>
2020-01-21 00:53:24 +0000

    created separate filter menu component

---
+++ src/components/Filter.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+
+const Filter = ({name, image, number}) => {
+
+    return (
+        <div className="filters">
+            Filter Options:
+        </div>
+    )
+}
+
+export default Filter;
