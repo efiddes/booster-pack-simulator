87a95152d3793f54caf5f5b132380b99a8a8a632
Ethan Fiddes <efiddes@hotmail.co.uk>
2020-02-14 01:00:35 +0000

    Modified app to use my custom hook and able to convert the data structure into useable components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import './App.css';
 import Card from './components/Card';
 import Filter from './components/Filter';
 import Loader from './components/Loader';
-import GenerateBooster from './calls/booster.js';
+import { useGenerateCards } from './calls/booster.js';
 
 function App() {
 
   const standard = ["grn", "rna", "war", "m20", "eld"]; //, "thb"];
-  const [loading, setLoading] = useState(true);
-  const [booster, setBooster] = useState([]);
-  let quantityOf = { 
-    mythic: 0,
-    rare: 1,
-    uncommon: 3,
-    common: 10,
-    basic: 1 
-  };
+  let booster = [
+    {rarity: Math.random() > 0.125 ? 'rare' : 'mythic', pageSize: 1},
+    {rarity: 'uncommon' , pageSize: 3},
+    {rarity: 'common' , pageSize: 10},
+    {type: 'basic' , pageSize: 1}
+  ];
 
-  useEffect(() => {
-    GenerateBooster("eld", quantityOf, setBooster, setLoading);
-  }, []);
+  let urls = [];
 
+  booster.forEach(obj => {
+    let names = Object.getOwnPropertyNames(obj);
+    let values = Object.values(obj);
+    let url = `https://api.magicthegathering.io/v1/cards?set=${standard[standard.length -1]}`;
+
+    for (let i = 0; i < names.length; i++) {
+      url +=`&${names[i]}=${values[i]}`;
+    }
+
+    url += "&random=true&contains=imageUrl";
+    urls.push(url);
+  });
+
+  const [isLoading, fetchedData] = useGenerateCards(urls, []);
+  
   return (
     <div className="App">
       <header>
         <h2>Booster Pack Simulator</h2>
       </header>
       <Filter standard={standard}/>
-      {loading ? <Loader/> : booster.map(card => (
+      {isLoading ? <Loader/> : fetchedData.map(object => (object.cards).map(card => 
         <Card 
           key={card.number} 
           name={card.name} 
