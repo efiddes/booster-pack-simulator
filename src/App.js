b5bcbfbe33bd333c7cad5f634ae4dcad805087b7
Ethan Fiddes <efiddes@hotmail.co.uk>
2020-02-19 20:43:23 +0000

    imported the LoadingContextProvider

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Filter from './components/Filter';
 import Booster from './components/Booster';
 import BoosterContextProvider from './contexts/BoosterContext';
+import LoadingContextProvider from './contexts/LoadingContext';
 
 function App() {
   
@@ -14,8 +15,10 @@ function App() {
         <h2>Booster Pack Simulator</h2>
       </header>
       <BoosterContextProvider>
-        <Filter/>
-        <Booster />
+        <LoadingContextProvider>
+            <Filter/>
+            <Booster />
+        </LoadingContextProvider>
       </BoosterContextProvider>
       <footer>By Ethan Fiddes</footer>
     </div>
