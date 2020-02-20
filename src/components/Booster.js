342fa2c347e3342c1a86dc9c016d1d77e2e7f2dd
Ethan Fiddes <efiddes@hotmail.co.uk>
2020-02-20 16:09:14 +0000

    updated to make use of the new sanitised response data

--- src/components/Booster.js
+++ src/components/Booster.js
@@ -1,27 +1,29 @@
 import React, { useContext } from 'react';
 
+import './booster.css';
 import Card from './Card';
 import Loader from './Loader';
 import { BoosterContext } from '../contexts/BoosterContext';
 import { LoadingContext } from '../contexts/LoadingContext';
 import { useGenerateCards } from '../calls/booster.js';
 
-
 const Booster = () => {
 
     const { boosterPack } = useContext(BoosterContext);
     const { isLoading } = useContext(LoadingContext);
-    const fetchedData = useGenerateCards(boosterPack, [boosterPack]);
+    const finalBooster = useGenerateCards(boosterPack, [boosterPack]);
 
-    return (
-        isLoading ? <Loader/> : fetchedData.map(object => (object.cards).map(card => 
-            <Card 
+    return (   
+        <div className="cards-container">
+        {isLoading && finalBooster.length ? <Loader/> : finalBooster.map(arr => (arr.cards).map(card => 
+            <Card
                 key={card.number} 
                 name={card.name} 
                 image={card.imageUrl}
                 number={card.number}
             />
-        ))
+        ))}
+        </div>
     );
 }
 
