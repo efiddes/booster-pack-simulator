77dd7c040b19e47b020268a4f8de13162c51aba1
Ethan Fiddes <efiddes@hotmail.co.uk>
2020-02-20 21:58:04 +0000

    extended the footer content to be more informative

--- src/App.js
+++ src/App.js
@@ -11,16 +11,30 @@ function App() {
   
   return (
     <div className="App">
-      <header>
-        <h2>Booster Pack Simulator</h2>
-      </header>
-      <BoosterContextProvider>
+        <header>
+            <h2>Booster Pack Simulator</h2>
+        </header>
+        <BoosterContextProvider>
         <LoadingContextProvider>
             <Filter/>
             <Booster />
         </LoadingContextProvider>
-      </BoosterContextProvider>
-      <footer>By Ethan Fiddes</footer>
+        </BoosterContextProvider>
+        <footer>
+            <ul class="footer">
+                <li>By <b>Ethan Fiddes</b></li>
+                <li>
+                    Built with: 
+                    <img className="react-logo" alt="built with React" src="/logo192.png"/>
+                    <a href="https://reactjs.org" target="_blank">
+                        <span className="react">React</span>
+                    </a> 
+                </li>
+                <li>
+                    Data provided from <a href="https://docs.magicthegathering.io/" target="_blank">Magic: The Gathering API</a>
+                </li>
+            </ul>
+        </footer>
     </div>
   );
 }
